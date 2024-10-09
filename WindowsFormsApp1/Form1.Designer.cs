49fb5f53d4491243d37d301d5dfdc2ba4b252f68
ou mai goss <haison27102003@gmail.com>
2024-10-09 22:46:46 +0700

    Day code len nhanh main

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnchia = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.btncong = new System.Windows.Forms.Button();
+            this.btntru = new System.Windows.Forms.Button();
+            this.btnnhan = new System.Windows.Forms.Button();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnchia
+            // 
+            this.btnchia.Location = new System.Drawing.Point(615, 211);
+            this.btnchia.Name = "btnchia";
+            this.btnchia.Size = new System.Drawing.Size(75, 23);
+            this.btnchia.TabIndex = 0;
+            this.btnchia.Text = "/";
+            this.btnchia.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(325, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Máy tính";
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(169, 127);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 22);
+            this.textA.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Số B:";
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(169, 212);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 22);
+            this.textB.TabIndex = 2;
+            // 
+            // btncong
+            // 
+            this.btncong.Location = new System.Drawing.Point(416, 126);
+            this.btncong.Name = "btncong";
+            this.btncong.Size = new System.Drawing.Size(75, 23);
+            this.btncong.TabIndex = 0;
+            this.btncong.Text = "+";
+            this.btncong.UseVisualStyleBackColor = true;
+            this.btncong.Click += new System.EventHandler(this.btncong_Click);
+            // 
+            // btntru
+            // 
+            this.btntru.Location = new System.Drawing.Point(416, 221);
+            this.btntru.Name = "btntru";
+            this.btntru.Size = new System.Drawing.Size(75, 23);
+            this.btntru.TabIndex = 0;
+            this.btntru.Text = "-";
+            this.btntru.UseVisualStyleBackColor = true;
+            // 
+            // btnnhan
+            // 
+            this.btnnhan.Location = new System.Drawing.Point(615, 124);
+            this.btnnhan.Name = "btnnhan";
+            this.btnnhan.Size = new System.Drawing.Size(75, 23);
+            this.btnnhan.TabIndex = 0;
+            this.btnnhan.Text = "*";
+            this.btnnhan.UseVisualStyleBackColor = true;
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(169, 296);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(100, 22);
+            this.textResult.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Kết quả:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnnhan);
+            this.Controls.Add(this.btntru);
+            this.Controls.Add(this.btncong);
+            this.Controls.Add(this.btnchia);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnchia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.Button btncong;
+        private System.Windows.Forms.Button btntru;
+        private System.Windows.Forms.Button btnnhan;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
